409b27d771c52909b412d3e74512bdc5a3571d9a
jlee7362 <jlee736222@gmail.com>
2025-04-16 19:33:24 +0900

    게시글 작성기능 피상적 구현

--- src/main/java/koreaIT/Main.java
+++ src/main/java/koreaIT/Main.java
@@ -7,15 +7,32 @@ public class Main {
 
         System.out.println("===프로그램 시작===");
 
+        int lastId = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.print("명령어) ");
+        while (true){
+            System.out.print("명령어) ");
+            String cmd = sc.nextLine();
 
-        String cmd = sc.nextLine();
+            if(cmd.equals("article list")){
+                System.out.println("번호  /  제목  /  내용");
+            }
+            else if(cmd.equals("article write")){
+                System.out.print("제목 : ");
+                String title = sc.nextLine();
+                System.out.print("내용 : ");
+                String body = sc.nextLine();
 
-        System.out.println(cmd);
+                lastId++;
 
-        if(cmd.equals("exit")){
-            System.out.println("=====프로그램 종료=====");
+                System.out.printf("%d번 글이 생성되었습니다.\n",lastId);
+            }
+            else if(cmd.equals("exit")){
+                System.out.println("=====프로그램 종료=====");
+                break;
+            }
+            else{
+                System.out.println("사용할 수 없는 명령어입니다.");
+            }
         }
 
     }
