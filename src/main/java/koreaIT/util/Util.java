8ac6818a457e1f98dba39db327fa380e2111de18
jlee7362 <jlee736222@gmail.com>
2025-04-22 19:49:27 +0900

    리팩토링 중1

--- src/main/java/koreaIT/util/Util.java
+++ src/main/java/koreaIT/util/Util.java
@@ -1,11 +1,7 @@
-package koreaIT;
+package koreaIT.util;
 
-import java.lang.reflect.Member;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class Util {
 
